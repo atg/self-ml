335f064843017bbec9d26037360bcb49dcf8c70c
Alex Gordon <alextgordon@gmail.com>
2010-12-08 11:19:17 +0000

    Fixed SFNodeReplaceChildAtIndexWithLast for nodes with 2 children

--- Implementations/Objective-C/Source/SFONode.m
+++ Implementations/Objective-C/Source/SFONode.m
@@ -213,16 +213,7 @@
 	if ([b respondsToSelector:@selector(setNodeRef:)])
 		[b setNodeRef:SFNullNode];
 	
-    printf("BEFORE: ");
-    SFNodePrintRepresentation(node);
-    printf("\n\n");
-    
 	SFNodeReplaceChildAtIndexWithLast(node, index);
-	
-    printf("AFTER: ");
-    SFNodePrintRepresentation(node);
-    printf("\n\n");
-    
     
 	if ([self childCount] <= 1)
 	{
@@ -231,7 +222,6 @@
 	
 	if (newChild != nil)
 	{
-        printf("REPLACING\n");
 		[children replaceObjectAtIndex:index withObject:[children lastObject]];
 		[children removeLastObject];
 	}
